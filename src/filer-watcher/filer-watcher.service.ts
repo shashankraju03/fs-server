b76d437fe2ff138cd08aaea282753a5db33457ae
Shashank Raju <shashankrajuprathikantam@gmail.com>
2024-12-13 20:54:40 +0530

    improvements

--- src/filer-watcher/filer-watcher.service.ts
+++ src/filer-watcher/filer-watcher.service.ts
@@ -7,68 +7,61 @@ import { SocketGateway } from 'src/socket-gateway/socket-gateway.gateway';
 export class FilerWatcherService implements OnModuleInit, OnModuleDestroy {
   constructor(private readonly socketGateway: SocketGateway) {}
 
+  private eofPointer: number = 0;
+  private filePath = path.join(__dirname, '../../files/example.txt');
+
   onModuleInit() {
-    this.startReadingFile();
+    this.startWatchingFile();
   }
 
   onModuleDestroy() {
-    this.stopReadingFile();
+    this.stopWatchingFile();
   }
 
-  private eofPointer: number = 0;
-
-  private filePath = path.join(__dirname, '../../files/example.txt');
-
-  private intervalId: NodeJS.Timeout;
-
-  private startReadingFile() {
+  private startWatchingFile() {
     if (!fs.existsSync(this.filePath)) {
       console.error(`File not found: ${this.filePath}`);
       return;
     }
 
-    console.log('Starting file read operation....');
+    console.log('Starting to watch file for changes...');
 
-    this.intervalId = setInterval(() => {
-      this.readFileChunk();
-    }, 500);
+    fs.watchFile(this.filePath, { interval: 500 }, (curr, prev) => {
+      console.log('File changed. Reading new content...');
+      this.readFileChunk(curr, prev);
+    });
   }
 
-  private stopReadingFile() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-      console.log('Stopped file read operation.');
-    }
+  private stopWatchingFile() {
+    fs.unwatchFile(this.filePath);
+    console.log('Stopped watching file for changes.');
   }
 
-  private readFileChunk() {
-    const fileStats = fs.statSync(this.filePath);
-    // console.log(`File size: ${fileStats.size}, eofPointer: ${this.eofPointer}`);
-
-    if (this.eofPointer >= fileStats.size) {
+  private readFileChunk(curr: fs.Stats, prev: fs.Stats) {
+    // Ensure there's new data to read
+    if (prev.size >= curr.size) {
       console.log('No new data to read.');
       return;
     }
 
-    const stream = fs.createReadStream(this.filePath, {
-      start: this.eofPointer,
-      encoding: 'utf-8',
-    });
+    if (curr.size >= prev.size) {
+      const stream = fs.createReadStream(this.filePath, {
+        start: prev.size,
+        encoding: 'utf-8',
+      });
 
-    console.log('Stream created. Waiting for data...');
+      stream.on('data', (chunk) => {
+        console.log(`Read chunk: ${chunk}`);
+        this.socketGateway.emitMessage(chunk.toString());
+      });
 
-    stream.on('data', (chunk) => {
-      console.log(`Read chunk: ${chunk}`);
-      this.eofPointer += Buffer.byteLength(chunk, 'utf-8');
-      this.socketGateway.emitMessage(chunk.toString());
-    });
+      stream.on('error', (err) => {
+        console.error('Error reading file:', err.message);
+      });
 
-    stream.on('error', (err) => {
-      console.error('Error reading file:', err.message);
-    });
-
-    stream.on('end', () => {
-      console.log('Reached end of current file content.');
-    });
+      stream.on('end', () => {
+        console.log('Reached end of current file content.');
+      });
+    }
   }
 }
